2b76b6980c472f17ef5f0772e2b2651dbd6f5bf2
Paramet Kongjaroen <parametkongjaroen@Paramet-Kongjaroen.local>
2022-12-07 13:12:07 +0700

    update

--- core/components/mission/Pages/InProgressPage.tsx
+++ core/components/mission/Pages/InProgressPage.tsx
@@ -5,12 +5,6 @@ export const InProgressPage = () => {
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-      </div>
-      <p className="font-bold mt-10 mb-2">Wolf Door Mission Access</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-5">
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
         <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
