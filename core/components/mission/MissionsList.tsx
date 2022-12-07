fb5ea87db5598cc995b5dbe8be33a96f9a6d330c
Paramet Kongjaroen <parametkongjaroen@Paramet-Kongjaroen.local>
2022-12-07 18:24:14 +0700

    update layout mission

--- core/components/mission/MissionsList.tsx
+++ core/components/mission/MissionsList.tsx
@@ -30,40 +30,40 @@ export const MissionList = () => {
     <>
       <div className="flex flex-row justify-center mt-12">
         <div
-          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#5051E2] hover:text-white rounded-full items-center cursor-pointer "
+          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#E2211C] hover:text-white rounded-full items-center cursor-pointer "
           onClick={() => nextPageNumber("1")}
           style={{
-            backgroundColor: page === "pageone" ? "#5051E2" : "",
+            backgroundColor: page === "pageone" ? "#E2211C" : "",
             color: page === "pageone" ? "#ffff" : "",
           }}
         >
           <a className="text-sm md:text-xl">Available</a>
         </div>
         <div
-          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#5051E2] hover:text-white rounded-full items-center cursor-pointer "
+          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#E2211C] hover:text-white rounded-full items-center cursor-pointer "
           onClick={() => nextPageNumber("2")}
           style={{
-            backgroundColor: page === "pagetwo" ? "#5051E2" : "",
+            backgroundColor: page === "pagetwo" ? "#E2211C" : "",
             color: page === "pagetwo" ? "#ffff" : "",
           }}
         >
           <a className="text-sm md:text-xl">In Progress</a>
         </div>
         <div
-          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#5051E2] hover:text-white rounded-full items-center cursor-pointer "
+          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#E2211C] hover:text-white rounded-full items-center cursor-pointer "
           onClick={() => nextPageNumber("3")}
           style={{
-            backgroundColor: page === "pagethree" ? "#5051E2" : "",
+            backgroundColor: page === "pagethree" ? "#E2211C" : "",
             color: page === "pagethree" ? "#ffff" : "",
           }}
         >
           <a className="text-sm md:text-xl">Completed</a>
         </div>
         <div
-          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#5051E2] hover:text-white rounded-full items-center cursor-pointer "
+          className="px-2 py-2 md:py-3 md:px-5 hover:bg-[#E2211C] hover:text-white rounded-full items-center cursor-pointer "
           onClick={() => nextPageNumber("4")}
           style={{
-            backgroundColor: page === "pagefour" ? "#5051E2" : "",
+            backgroundColor: page === "pagefour" ? "#E2211C" : "",
             color: page === "pagefour" ? "#ffff" : "",
           }}
         >
--- core/components/mission/Pages/AvailablePage.tsx
+++ core/components/mission/Pages/AvailablePage.tsx
@@ -1,13 +1,50 @@
+import Link from "next/link";
+
 export const AvailablePage = () => {
+  const mockId = [1, 2, 3, 4, 5, 6];
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-5">
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
-        <div className="h-40 w-auto bg-slate-300 rounded-2xl "></div>
+        {mockId.map((item) => {
+          return (
+            <Link href={`/missions/${item}`} key={item}>
+              <div className="flex h-max bg-slate-300 rounded-2xl md:transform md:transition md:duration-300 md:hover:scale-110	cursor-pointer">
+                <div className="w-7/12 md:w-7/12 mx-0 md:mx-5">
+                  <div className="flex flex-col py-3 px-5 md:px-0">
+                    <p className="font-bold text-sm ">TrueID</p>
+                    <p className="font-bold text-sm ">Watch to earn</p>
+                    <p className="font-bold text-sm ">World Cup 2022 match</p>
+                  </div>
+                  <div className="px-5 md:px-0">
+                    <div className="w-full bg-black	rounded-full h-2.5 mt-1 mb-3 ">
+                      <div
+                        className="bg-[#E2211C] h-2.5 rounded-full"
+                        style={{ width: `75%` }}
+                      ></div>
+                      <p className="text-sm mt-1">1304/2500 Completed</p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row pt-6 mb-2 justify-center">
+                    <div className="bg-white mr-2  px-1 py-1 md:px-5 md:py-2 rounded-2xl">
+                      <p className="text-sm">NFT</p>
+                    </div>
+                    <div className="bg-white ml-2 px-1 py-1 md:px-2 md:py-2  rounded-2xl">
+                      <p className="text-sm">DISCOUNT CODE</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-5/12 md:w-5/12 flex justify-center md:justify-end px-2 py-4 md:py-2">
+                  <img
+                    src="assets/franc.png"
+                    alt="base image"
+                    className="rounded-2xl object-cover"
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
